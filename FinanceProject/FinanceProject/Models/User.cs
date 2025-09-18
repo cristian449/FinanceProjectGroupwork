517e19eb9942c26dadd173232f78b173993139cc
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-09-18 09:19:28 +0300

    First commit for Login/Register
    Added Viewmodels folder as well as the RegisterViewmodel and LoginViewmodel classes and needed login information into them with data annotations, added User.cs class into models folder as well as added on package Aspnet.identity

---
+++ FinanceProject/FinanceProject/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace FinanceProject.Models
+{
+    public class User : IdentityBuilder<Guid>
+    {
+
+    }
+}
---
+++ FinanceProject/FinanceProject/Viewmodels/RegisterViewmodel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceProject.Viewmodels
+{
+    public class RegisterViewmodel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string DateOfBirth { get; set; } //For now leavin it as string may change to Datetime later
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
