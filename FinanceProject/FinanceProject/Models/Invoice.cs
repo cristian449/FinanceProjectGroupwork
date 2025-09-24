bc96d350b07afea8388007acff4918c81523befc
arturmuursepp <artur.ateam@gmail.com>
2025-09-25 01:39:04 +0300

    Added model, view and controller for Invoices

---
+++ FinanceProject/FinanceProject/Models/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceProject.Models
+{
+    public class Invoice // võime ka nimetada üle transaction'iks kui tahate
+    {
+        public int InvoiceID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        public string Header { get; set; } // aka title
+        [StringLength(200)]
+        public string Description { get; set; }
+        public float Amount { get; set; } // money
+        public DateTime InvoiceDate { get; set; }
+
+    }
+}
