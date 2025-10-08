f00bcb12461a3e083baa329c33caeec0bea7599c
arturmuursepp <artur.ateam@gmail.com>
2025-10-08 15:29:50 +0300

    Made small changes to all views, added view and methods for Editing (works) and Delete view (incomplete)

--- FinanceProject/FinanceProject/Controllers/InvoicesController.cs
+++ FinanceProject/FinanceProject/Controllers/InvoicesController.cs
@@ -2,6 +2,7 @@
 using FinanceProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Threading.Tasks;
 
 namespace FinanceProject.Controllers
@@ -62,6 +63,81 @@ namespace FinanceProject.Controllers
             }
 
             return View(invoice);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices.FirstOrDefaultAsync(
+                i => i.InvoiceID == id);
+
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+        }
+        [HttpPost, ActionName("Edit")]
+        public async Task<IActionResult> EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var invoiceToUpdate = await _context.Invoices.FirstOrDefaultAsync(i => i.InvoiceID == id);
+            if (await TryUpdateModelAsync<Invoice>(invoiceToUpdate, "",
+                i => i.Header,
+                i => i.Description,
+                i => i.InvoiceCategory,
+                i => i.Amount))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes" +
+                    "Try again, and if the problem persists" +
+                    "see your system administrator");
+                }
+            }
+            return View(invoiceToUpdate);
+        }
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var invoice = await _context.Invoices.FirstOrDefaultAsync(
+                i => i.InvoiceID == id);
+
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            return View(invoice);
+
+            //_context.Remove(invoice);
+            //await _context.SaveChangesAsync();
+
+            //if (saveChangesError.GetValueOrDefault())
+            //{
+            //    ViewData["ErrorMessage"] =
+            //        "Delete failed. Try again, and if problem persists" +
+            //        "see your system administrator";
+            //}
+
+            //return RedirectToAction(nameof(Index));
         }
     }
 }
