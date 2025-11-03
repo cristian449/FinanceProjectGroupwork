5fa0bb9c90bb9fe8fef7499b8812de1da2431f67
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-11-03 13:57:55 +0200

    QoL commit
    Added comment into AdminController, added title to UserManaging , added text beneath Birthdate input

--- FinanceProject/FinanceProject/Controllers/AdminController.cs
+++ FinanceProject/FinanceProject/Controllers/AdminController.cs
@@ -59,6 +59,10 @@ namespace FinanceProject.Controllers
 
 
 
+        // At the current moment this only promotes users to admin yes, however does not allow the user to log in as admin as the adminkey claim
+        // no work
+        // Technically the User could still be an Admin as long as they dont log out, but once they log out they cant log back in as admin
+
 
         [HttpPost]
         public async Task<IActionResult> ChangeRole(Guid id)
